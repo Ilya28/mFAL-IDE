7509cea588a07e16d42784cd3396253afad76cdc
ilya28rut <ilya28rut@gmail.com>
2020-01-31 18:28:42 +0200

    Add "res" folder and CSS files. Program GUI development.

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -1,23 +1,25 @@
-package sample;
+package application;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class Main extends Application {
 
-
     @Override
     public void start(Stage primaryStage) throws Exception{
-
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("mFAL IDE");
+        primaryStage.setScene(new Scene(root /*, 900, 600*/));
         primaryStage.show();
-    }
+      //  primaryStage.
 
+    }
 
     public static void main(String[] args) {
         launch(args);
